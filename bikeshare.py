6f33dfa9516f0ab861e5d4f9e2b0b32cc6ab4414
Truc Bui <buit1209@gmail.com>
2020-11-14 17:57:04 +0200

    Edit trip_duration_stats desciption in bikeshare

--- bikeshare.py
+++ bikeshare.py
@@ -144,7 +144,7 @@ def station_stats(df):
 
 
 def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
+    """Displays Statistics On the Total and Average Trip Duration."""
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
